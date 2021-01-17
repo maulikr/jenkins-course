22c70ed9a512c9f89c14c58e28c552611bc7b3fe
Maulik Rawal <rmaulik@gmail.com>
2021-01-16 20:37:49 -0500

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('maulikr/docker-nodejs-demo')
+            repositoryName('maulikrawal/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
